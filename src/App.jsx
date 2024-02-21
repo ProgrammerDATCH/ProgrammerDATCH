9eae1805f2c109a06ac562eab03ca8f4044e3652
ProgrammerDATCH <programmerdatch@gmail.com>
2024-02-21 08:31:01 +0200

    #5

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
         </div>
         <About />
         <Experience />
-        <Tech />
+        {/* <Tech /> */}
         <Works />
         <Feedbacks />
         <div className='relative z-0'>
